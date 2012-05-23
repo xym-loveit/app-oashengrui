6e493a22080ef2afa234874b53012b2df51858a7
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-05-23 18:26:09 +0000

    dwz used to render the UI page and menu created with icons.

---
+++ trunk/src/main/java/org/shengrui/oa/web/action/index/IndexAction.java
@@ -0,0 +1,31 @@
+package org.shengrui.oa.web.action.index;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.shengrui.oa.web.action.BaseAction;
+
+/**
+ * The index web action, it shows the index page for system.
+ * 
+ * @author  Jeccy.Zhao
+ *
+ */
+public class IndexAction 
+extends BaseAction
+{
+	/**
+	 * <b>[WebAction]</b> 
+	 * <br/>
+	 * The index presentation page action.
+	 */
+	public ActionForward index (ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) 
+	{
+		return mapping.findForward("index");
+	}
+	
+}
