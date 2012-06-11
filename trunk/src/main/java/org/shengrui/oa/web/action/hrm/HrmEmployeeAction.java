88f1f1a8ac1a8573fa137424ad9dc618cd5db3ac
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-06-11 08:52:08 +0000

    Add Employee Document Action

--- trunk/src/main/java/org/shengrui/oa/web/action/hrm/HrmEmployeeAction.java
+++ trunk/src/main/java/org/shengrui/oa/web/action/hrm/HrmEmployeeAction.java
@@ -1,5 +1,18 @@
 package org.shengrui.oa.web.action.hrm;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.shengrui.oa.model.hrm.ModelHrmEmployee;
+
+import cn.trymore.core.exception.ServiceException;
+import cn.trymore.core.web.paging.PaginationSupport;
+import cn.trymore.core.web.paging.PagingBean;
+
 /**
  * 员工档案
  * 
@@ -9,5 +22,42 @@ package org.shengrui.oa.web.action.hrm;
 public class HrmEmployeeAction
 extends BaseHrmAction
 {
+	/**
+	 * The LOGGER
+	 */
+	private static final Logger LOGGER = Logger.getLogger(HrmHireAction.class);
 	
+	/**
+	 * <b>[WebAction]</b> <br/>
+	 * 员工档案管理
+	 */
+	public ActionForward hrmEmployeeDocIndex(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) 
+	{
+		
+		try
+		{
+			ModelHrmEmployee formEmployeeInfo = (ModelHrmEmployee) form;
+			
+			PagingBean pagingBean = this.getPagingBean(request);
+			PaginationSupport<ModelHrmEmployee> employeeInfo =
+					this.serviceHrmEmployee.getEmployeeInfoPagination(formEmployeeInfo, pagingBean);
+			
+			request.setAttribute("employeeInfo", employeeInfo);
+			request.setAttribute("employeeForm", formEmployeeInfo);
+			
+			// 获取所有校区, 用于搜索查询使用
+			request.setAttribute("districts", this.serviceSchoolDistrict.getAll());
+			
+			// 输出分页信息至客户端
+			outWritePagination(request, pagingBean, employeeInfo);
+			
+		} 
+		catch (ServiceException e)
+		{
+			LOGGER.error("Exception raised when fetch all employee documents.", e);
+		}
+		
+		return mapping.findForward("hrm.page.employee.doc.index");
+	}
 }
