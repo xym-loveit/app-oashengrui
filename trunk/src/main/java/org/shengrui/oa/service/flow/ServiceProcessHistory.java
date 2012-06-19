f4b8d39c5e1b03c1859768b055a04f1201273c37
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-06-19 09:25:15 +0000

    process history interface updated.

--- trunk/src/main/java/org/shengrui/oa/service/flow/ServiceProcessHistory.java
+++ trunk/src/main/java/org/shengrui/oa/service/flow/ServiceProcessHistory.java
@@ -1,7 +1,10 @@
 package org.shengrui.oa.service.flow;
 
+import java.util.List;
+
 import org.shengrui.oa.model.flow.ModelProcessHistory;
 
+import cn.trymore.core.exception.ServiceException;
 import cn.trymore.core.service.ServiceGeneric;
 
 /**
@@ -13,5 +16,23 @@ import cn.trymore.core.service.ServiceGeneric;
 public interface ServiceProcessHistory
 extends ServiceGeneric<ModelProcessHistory>
 {
-
+	/**
+	 * Obtains list of process histories with the specified process form no. 
+	 * 
+	 * @param formNo
+	 *         the process form no.
+	 * @return list of process histories.
+	 * @throws ServiceException
+	 */
+	List<ModelProcessHistory> getProcessHistoryByFormNo (String formNo) throws ServiceException;
+	
+	/**
+	 * Obtains list of process histories with the specified user id.
+	 * 
+	 * @param userId
+	 *          the user id
+	 * @return list of process histories
+	 * @throws ServiceException
+	 */
+	List<ModelProcessHistory> getProcessHistoryByUser (String userId) throws ServiceException;
 }
--- trunk/src/main/java/org/shengrui/oa/service/flow/impl/ServiceProcessHistoryImpl.java
+++ trunk/src/main/java/org/shengrui/oa/service/flow/impl/ServiceProcessHistoryImpl.java
@@ -1,21 +1,70 @@
 package org.shengrui.oa.service.flow.impl;
 
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.shengrui.oa.dao.flow.DAOProcessHistory;
 import org.shengrui.oa.model.flow.ModelProcessHistory;
 import org.shengrui.oa.service.flow.ServiceProcessHistory;
 
+import cn.trymore.core.exception.ServiceException;
 import cn.trymore.core.service.impl.ServiceGenericImpl;
 
 public class ServiceProcessHistoryImpl
 extends ServiceGenericImpl<ModelProcessHistory> implements ServiceProcessHistory
 {
-	
+	/**
+	 * The process history repository
+	 */
 	private DAOProcessHistory daoProcessHistory;
 	
 	public ServiceProcessHistoryImpl(DAOProcessHistory dao)
 	{
 		super(dao);
 		this.daoProcessHistory = dao;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.shengrui.oa.service.flow.ServiceProcessHistory#getProcessHistoryByFormNo(java.lang.String)
+	 */
+	@Override
+	public List<ModelProcessHistory> getProcessHistoryByFormNo(String formNo)
+			throws ServiceException
+	{
+		try
+		{
+			DetachedCriteria criteria = DetachedCriteria.forClass(ModelProcessHistory.class);
+			criteria.add(Restrictions.eq("applyFormNo", formNo));
+			criteria.addOrder(Order.asc("sortCode"));
+			return this.daoProcessHistory.getListByCriteria(criteria);
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e);
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.shengrui.oa.service.flow.ServiceProcessHistory#getProcessHistoryByUser(java.lang.String)
+	 */
+	@Override
+	public List<ModelProcessHistory> getProcessHistoryByUser(String userId)
+			throws ServiceException
+	{
+		try
+		{
+			DetachedCriteria criteria = DetachedCriteria.forClass(ModelProcessHistory.class);
+			criteria.add(Restrictions.in("auditUserIds", userId.split(",")));
+			return this.daoProcessHistory.getListByCriteria(criteria);
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e);
+		}
 	}
 
 	public void setDaoProcessHistory(DAOProcessHistory daoProcessHistory)
