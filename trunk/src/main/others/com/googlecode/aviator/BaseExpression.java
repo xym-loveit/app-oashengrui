7dbe3af1d700859a5d53ca31a81817a9dc00e80e
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-06-27 06:25:05 +0000

    aviator source codes moved into project since NoClassDefinition raised when jar imported.

---
+++ trunk/src/main/others/com/googlecode/aviator/BaseExpression.java
@@ -0,0 +1,52 @@
+package com.googlecode.aviator;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+
+/**
+ * Base expression
+ * 
+ * @author dennis
+ * 
+ */
+public abstract class BaseExpression implements Expression {
+
+    private List<String> varNames;
+
+
+    public BaseExpression(List<String> varNames) {
+        super();
+        LinkedHashSet<String> tmp = new LinkedHashSet<String>(varNames.size());
+        // process nested names
+        for (String name : varNames) {
+            if (name.contains(".")) {
+                name = name.substring(0, name.indexOf("."));
+            }
+            tmp.add(name);
+        }
+        this.varNames = new ArrayList<String>(tmp);
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.googlecode.aviator.IExpression#execute()
+     */
+    public Object execute() {
+        return this.execute(null);
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.googlecode.aviator.IExpression#getVariableNames()
+     */
+    public List<String> getVariableNames() {
+        return this.varNames;
+    }
+
+}
