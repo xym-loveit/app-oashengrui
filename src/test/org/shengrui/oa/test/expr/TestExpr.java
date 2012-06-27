fc5799f301e5aec58d8fa876f7843b4cc61ec7a8
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-06-27 06:33:31 +0000

    project files updated

--- src/test/org/shengrui/oa/test/expr/TestExpr.java
+++ src/test/org/shengrui/oa/test/expr/TestExpr.java
@@ -1,19 +1,22 @@
 package org.shengrui.oa.test.expr;
 
-import com.greenpineyu.fel.FelEngine;
-import com.greenpineyu.fel.FelEngineImpl;
-import com.greenpineyu.fel.context.FelContext;
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Expression;
+
+import java.util.Map;
+import java.util.HashMap;
 
 public class TestExpr
 {
 	
 	public static void main(String[] args)
 	{
-		FelEngine fel = new FelEngineImpl();
-		FelContext cxt = fel.getContext();
-		cxt.set("x", 100);
-		System.out.println(fel.eval("x > 0 && x < 1000"));
+		Expression expr = AviatorEvaluator.compile("x > 0 && x < 1000");
+		Map<String, Object> env = new HashMap<String, Object>();
+		env.put("x", 340);
 		
+		Boolean result = (Boolean)expr.execute(env);
+		System.out.println(result);
 	}
 	
 }
