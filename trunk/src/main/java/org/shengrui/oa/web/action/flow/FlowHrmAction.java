7ca1269922651f5554dbe99c173bb6b817df2cd2
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-07-17 15:31:21 +0000

    action for HRM flow configuration re-updated to head version. (It's not accepted to modify other's code without any inform and code roll back....)

--- trunk/src/main/java/org/shengrui/oa/web/action/flow/FlowHrmAction.java
+++ trunk/src/main/java/org/shengrui/oa/web/action/flow/FlowHrmAction.java
@@ -1,6 +1,14 @@
 package org.shengrui.oa.web.action.flow;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import cn.trymore.core.exception.ServiceException;
 
 /**
  * 工作流设置 - 人资审批流程设置
@@ -21,7 +29,29 @@ extends FlowBaseAction
 	 */
 	public static final String PROCESS_TYPE_SLUG = "hrm";
 	
-	
+	/**
+	 * <b>[WebAction]</b> 
+	 * <br/>
+	 *财务审批流程页面
+	 */
+	public ActionForward pageFlowIndex (ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) 
+	{
+		
+		request.setAttribute("typeSlug", PROCESS_TYPE_SLUG);
+		
+		try
+		{
+			request.setAttribute("rootTypes", 
+					this.getProcessRootTypesBySlug(PROCESS_TYPE_SLUG));
+		} 
+		catch (ServiceException e)
+		{
+			LOGGER.error("Exception raised when obtain the root types with type:" + PROCESS_TYPE_SLUG, e);
+		}
+		
+		return mapping.findForward("page.sys.setting.flow.hrm.index");
+	}
 	
 	public static Logger getLogger()
 	{
