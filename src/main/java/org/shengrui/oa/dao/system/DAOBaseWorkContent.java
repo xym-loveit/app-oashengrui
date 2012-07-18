a8ba822cdecdb60ee521f33269a784f7a63bf729
tractorzjut <tractorzjut@gmail.com>
2012-07-18 06:04:12 +0000

    add DAO and DAO implement

---
+++ src/main/java/org/shengrui/oa/dao/system/DAOBaseWorkContent.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.shengrui.oa.dao.system;
+
+import org.shengrui.oa.model.system.ModelBaseWorkContent;
+
+import cn.trymore.core.dao.DAOGeneric;
+
+/**
+ * @author Tank.L
+ *
+ */
+public interface DAOBaseWorkContent extends DAOGeneric<ModelBaseWorkContent> {
+
+}
