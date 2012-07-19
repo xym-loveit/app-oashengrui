a88985f0beafd69e7a1681d71ca403766ebc78b5
tractorzjut@gmail.com <tractorzjut@gmail.com@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-07-19 10:58:15 +0000

    modify a function

--- trunk/src/main/java/org/shengrui/oa/dao/system/impl/DAOWorkTemplateImpl.java
+++ trunk/src/main/java/org/shengrui/oa/dao/system/impl/DAOWorkTemplateImpl.java
@@ -4,29 +4,32 @@ import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
-import org.shengrui.oa.dao.system.DAOSchoolDepartment;
-import org.shengrui.oa.model.system.ModelSchoolDepartment;
+import org.shengrui.oa.dao.system.DAOWorkTemplate;
+import org.shengrui.oa.model.system.ModelWorkTemplate;
 
 import cn.trymore.core.dao.impl.DAOGenericImpl;
 import cn.trymore.core.exception.DAOException;
 
 public class DAOWorkTemplateImpl
-extends DAOGenericImpl<ModelSchoolDepartment> implements DAOSchoolDepartment
+extends DAOGenericImpl<ModelWorkTemplate> implements DAOWorkTemplate
 {
 	
 	/*
 	 * (non-Javadoc)
-	 * @see org.shengrui.oa.dao.system.DAOSchoolDepartment#getDepartmentByOrganization(java.lang.Integer)
+	 * @see org.shengrui.oa.dao.system.DAOWorkTemplate#getWorkArrangesByTemplateIdAndDay(java.lang.String,java.lang.String)
 	 */
 	@Override
-	public List<ModelSchoolDepartment> getDepartmentByOrganization(
-			Integer orgType) throws DAOException
+	public List<ModelWorkTemplate> getWorkArrangesByCriteria(
+			ModelWorkTemplate entity) throws DAOException
 	{
-		DetachedCriteria criteria = DetachedCriteria.forClass(ModelSchoolDepartment.class);
+		DetachedCriteria criteria = DetachedCriteria.forClass(ModelWorkTemplate.class);
 		
-		if (orgType != null)
+		if (entity != null && entity.getTemplateId()!=null)
 		{
-			criteria.add(Restrictions.eq("depOrgType", orgType));
+			criteria.add(Restrictions.eq("templateId", entity.getTemplateId()));
+		}
+		if(entity != null && entity.getDistrict().getId()!=null){
+			criteria.createCriteria("district").add(Restrictions.eq("id", entity.getDistrict().getId()));
 		}
 		
 		return this.getListByCriteria(criteria);
