e701dd8861894cab9a29883fb100622319bd6eb5
tractorzjut <tractorzjut@gmail.com>
2012-07-20 10:56:33 +0000

    员工考勤服务接口及其实现

---
+++ src/main/java/org/shengrui/oa/service/admin/ServiceStaffAttendance.java
@@ -0,0 +1,33 @@
+package org.shengrui.oa.service.admin;
+
+import org.shengrui.oa.model.admin.ModelStaffAttendance;
+
+import cn.trymore.core.exception.ServiceException;
+import cn.trymore.core.service.ServiceGeneric;
+import cn.trymore.core.web.paging.PaginationSupport;
+import cn.trymore.core.web.paging.PagingBean;
+
+/**
+ * @author tank
+ *
+ */
+public interface ServiceStaffAttendance extends ServiceGeneric<ModelStaffAttendance> {
+	
+	/**
+	 * 根据id得到一条考勤记录
+	 * @author Tang 
+	 */
+	public ModelStaffAttendance getById(String id)throws ServiceException;
+	
+	/**
+	 * 根据信息查分页数据
+	 * 
+	 * @param entity
+	 * @param pagingBean
+	 * @return
+	 * @throws ServiceException
+	 */
+	PaginationSupport<ModelStaffAttendance> getPaginationByEntity (ModelStaffAttendance entity, 
+			PagingBean pagingBean) throws ServiceException;
+	
+}
