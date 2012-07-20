b83590840c075c7ff6f482029f7d92166270f310
tractorzjut@gmail.com <tractorzjut@gmail.com@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-07-20 10:53:02 +0000

    员工考勤DAO及其实现

---
+++ trunk/src/main/java/org/shengrui/oa/dao/admin/DAOStaffAttendance.java
@@ -0,0 +1,14 @@
+package org.shengrui.oa.dao.admin;
+
+import org.shengrui.oa.model.admin.ModelStaffAttendance;
+
+import cn.trymore.core.dao.DAOGeneric;
+
+/**
+ * staff attendance
+ * @author tank
+ *
+ */
+public interface DAOStaffAttendance extends DAOGeneric<ModelStaffAttendance> {
+
+}
