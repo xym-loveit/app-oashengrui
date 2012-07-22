fe67936acca6d3db8a1b9c2e093104f2a694b174
tractorzjut@gmail.com <tractorzjut@gmail.com@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-07-22 16:49:04 +0000

    会议服务接口及其实现

---
+++ trunk/src/main/java/org/shengrui/oa/service/admin/ServiceConferenceInfo.java
@@ -0,0 +1,31 @@
+package org.shengrui.oa.service.admin;
+
+import org.shengrui.oa.model.admin.ModelConference;
+
+import cn.trymore.core.exception.ServiceException;
+import cn.trymore.core.service.ServiceGeneric;
+import cn.trymore.core.web.paging.PaginationSupport;
+import cn.trymore.core.web.paging.PagingBean;
+
+/**
+ * The service for job hire information.
+ * 
+ * @author Jeccy.Zhao
+ *
+ */
+public interface ServiceConferenceInfo
+extends ServiceGeneric<ModelConference>
+{
+	
+	/**
+	 * 根据岗位信息查分页数据
+	 * 
+	 * @param entity
+	 * @param pagingBean
+	 * @return
+	 * @throws ServiceException
+	 */
+	PaginationSupport<ModelConference> getPaginationByEntity (ModelConference entity, 
+			PagingBean pagingBean) throws ServiceException;
+	
+}
