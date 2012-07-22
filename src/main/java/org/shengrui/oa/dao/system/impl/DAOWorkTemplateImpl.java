2613d3dc43015f03d9f5d152f4cf386c922c81d5
tractorzjut <tractorzjut@gmail.com>
2012-07-22 11:05:29 +0000

    修正启动模板的SQL

--- src/main/java/org/shengrui/oa/dao/system/impl/DAOWorkTemplateImpl.java
+++ src/main/java/org/shengrui/oa/dao/system/impl/DAOWorkTemplateImpl.java
@@ -39,6 +39,8 @@ extends DAOGenericImpl<ModelWorkTemplate> implements DAOWorkTemplate
 	public int enableTemplate(String id) throws DAOException {
 		// TODO Auto-generated method stub
 		String sql = "update app_system_work_template set enable='1' where templateId='"+id+"'";
+		String sql2 = "update app_system_work_template set enable='0'";
+		this.execUpdateByNativeSQL(sql2);
 		return this.execUpdateByNativeSQL(sql);
 	}
 
