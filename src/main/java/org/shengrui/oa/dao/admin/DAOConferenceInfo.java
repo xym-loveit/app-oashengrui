702a34d11a6148eebf023713a6ca86ee406b0bd4
tractorzjut <tractorzjut@gmail.com>
2012-07-22 16:48:23 +0000

    会议DAO接口及其实现

---
+++ src/main/java/org/shengrui/oa/dao/admin/DAOConferenceInfo.java
@@ -0,0 +1,11 @@
+package org.shengrui.oa.dao.admin;
+
+import org.shengrui.oa.model.admin.ModelConference;
+
+import cn.trymore.core.dao.DAOGeneric;
+
+public interface DAOConferenceInfo
+extends DAOGeneric<ModelConference>
+{
+
+}
---
+++ src/main/java/org/shengrui/oa/dao/admin/impl/DAOConferenceInfoImpl.java
@@ -0,0 +1,12 @@
+package org.shengrui.oa.dao.admin.impl;
+
+import org.shengrui.oa.dao.admin.DAOConferenceInfo;
+import org.shengrui.oa.model.admin.ModelConference;
+
+import cn.trymore.core.dao.impl.DAOGenericImpl;
+
+public class DAOConferenceInfoImpl
+extends DAOGenericImpl<ModelConference> implements DAOConferenceInfo
+{
+
+}
