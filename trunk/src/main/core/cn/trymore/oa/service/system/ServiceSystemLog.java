7e46e6a232e72e436dc5adbb61d48e2f74fdce55
tractorzjut@gmail.com <tractorzjut@gmail.com@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-07-25 16:22:05 +0000

    增加记录和查看日志的服务

--- trunk/src/main/core/cn/trymore/oa/service/system/ServiceSystemLog.java
+++ trunk/src/main/core/cn/trymore/oa/service/system/ServiceSystemLog.java
@@ -1,10 +1,28 @@
 package cn.trymore.oa.service.system;
 
+import cn.trymore.core.exception.ServiceException;
 import cn.trymore.core.service.ServiceGeneric;
+import cn.trymore.core.web.paging.PaginationSupport;
+import cn.trymore.core.web.paging.PagingBean;
 import cn.trymore.oa.model.system.ModelSystemLog;
 
 public interface ServiceSystemLog
 extends ServiceGeneric<ModelSystemLog>
 {
-
+	/**
+	 * 根据id得到一条基础工作记录
+	 * @author Lee
+	 */
+	public ModelSystemLog getById(String id)throws ServiceException;
+	
+	/**
+	 * 根据信息查分页数据
+	 * 
+	 * @param entity
+	 * @param pagingBean
+	 * @return
+	 * @throws ServiceException
+	 */
+	PaginationSupport<ModelSystemLog> getPaginationByEntity (ModelSystemLog entity, 
+			PagingBean pagingBean) throws ServiceException;
 }
