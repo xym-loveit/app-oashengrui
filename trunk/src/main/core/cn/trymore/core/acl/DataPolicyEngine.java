4b266f91b90a522f741a7094d562cd94d4170392
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-08-02 09:30:02 +0000

    data policy solution added, not implemented yet (still on practice phase)

---
+++ trunk/src/main/core/cn/trymore/core/acl/DataPolicyEngine.java
@@ -0,0 +1,44 @@
+package cn.trymore.core.acl;
+
+@SuppressWarnings("rawtypes")
+public class DataPolicyEngine
+{
+	//本地线程，用于存储线程公共对象
+	private static ThreadLocal threadLocalSession = new ThreadLocal();
+	
+	/**
+	 * 获取本地执行线程
+	 *
+	 * @return ThreadLocal 
+	 * 
+	 */
+	public static ThreadLocal getThreadLocalSession() 
+	{
+		return threadLocalSession;
+	}
+	
+	/**
+	 * 获取在线程内设置存储的对象
+	 */
+	public static Object get() 
+	{
+		return threadLocalSession.get();
+	}
+	
+	/**
+	 * 在线程内设置存储对象
+	 */
+	@SuppressWarnings("unchecked")
+	public static void set(Object obj) 
+	{
+		threadLocalSession.set(obj);
+	}
+	
+	/**
+	 * 移除在线程内设置存储的对象
+	 */
+	public static void remove() 
+	{
+		threadLocalSession.remove();
+	}
+}
