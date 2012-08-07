7ac133af06720f9b571820c31fb83db0cf2bf4ee
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-08-07 02:57:51 +0000

    critical bug#64 fixed.

--- src/main/java/org/shengrui/oa/web/action/personal/MyTaskPlanAction.java
+++ src/main/java/org/shengrui/oa/web/action/personal/MyTaskPlanAction.java
@@ -392,11 +392,16 @@ extends BaseAppAction
 					entity.setTaskPlannedStartDate(formEntity.getTaskPlannedStartDate());
 					entity.setTaskPlannedEndDate(formEntity.getTaskPlannedEndDate());	
 					
-					if (entity.getAuditStatus() != null && 
+					if (formEntity.getAuditStatus() == null)
+					{
+						// 提交审核...
+						entity.setAuditStatus(null);
+					}
+					else if (entity.getAuditStatus() != null && 
 							ModelTaskPlan.ETaskApprovalStatus.RETURNED.getValue().equals(entity.getAuditStatus()))
 					{
 						// 修改已退回为待审批状态...
-						entity.setAuditStatus(null);
+						entity.setAuditStatus(ModelTaskPlan.ETaskApprovalStatus.RETURNED.getValue());
 					}
 				}
 				else
