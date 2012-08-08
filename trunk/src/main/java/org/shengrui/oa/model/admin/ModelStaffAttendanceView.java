5ba8203379b651f1d837b3ade1ce4379b3b1317a
tractorzjut@gmail.com <tractorzjut@gmail.com@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-08-08 15:38:28 +0000

    增加校区，部门字段，以及数据权限过滤

--- trunk/src/main/java/org/shengrui/oa/model/admin/ModelStaffAttendanceView.java
+++ trunk/src/main/java/org/shengrui/oa/model/admin/ModelStaffAttendanceView.java
@@ -1,5 +1,9 @@
 package org.shengrui.oa.model.admin;
 
+import org.shengrui.oa.util.AppUtil;
+
+import cn.trymore.core.acl.AclFilterAnnotation;
+
 /**
  * @author tank
  *
@@ -7,7 +11,29 @@ package org.shengrui.oa.model.admin;
 public class ModelStaffAttendanceView extends ModelStaffAttendance {
 
 	private static final long serialVersionUID = 1414275885329827174L;
+	/**
+	 * 数据权限过滤
+	 */
+	@AclFilterAnnotation(
+		fieldNames ={"district_id", "dep_id", "staff_id"}, 
+		fieldTypes ={AppUtil.DATA_POLICY_DISTRICT, AppUtil.DATA_POLICY_DEPARTMENT, AppUtil.DATA_POLICY_PERSONAL}
+	)
+	private String aclFilterFields;
 	
+	/**
+	 * @return the aclFilterFields
+	 */
+	public String getAclFilterFields() {
+		return aclFilterFields;
+	}
+
+	/**
+	 * @param aclFilterFields the aclFilterFields to set
+	 */
+	public void setAclFilterFields(String aclFilterFields) {
+		this.aclFilterFields = aclFilterFields;
+	}
+
 	/**
 	 * 联合主键
 	 */
