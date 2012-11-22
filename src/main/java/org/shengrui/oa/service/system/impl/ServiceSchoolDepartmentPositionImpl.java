e157e53d78d53b59633982ae52eee844e8f55bc4
liczhaoxiang <liczhaoxiang@91fb1029-c160-1fed-7307-2a92bd01b1e1>
2012-11-22 08:26:58 +0000

    bug88 fixed.

--- src/main/java/org/shengrui/oa/service/system/impl/ServiceSchoolDepartmentPositionImpl.java
+++ src/main/java/org/shengrui/oa/service/system/impl/ServiceSchoolDepartmentPositionImpl.java
@@ -2,12 +2,16 @@ package org.shengrui.oa.service.system.impl;
 
 import java.util.List;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.shengrui.oa.dao.system.DAOSchoolDepartmentPosition;
+import org.shengrui.oa.model.system.ModelAppRole;
 import org.shengrui.oa.model.system.ModelSchoolDepartmentPosition;
 import org.shengrui.oa.service.system.ServiceSchoolDepartmentPosition;
 
 import cn.trymore.core.exception.ServiceException;
 import cn.trymore.core.service.impl.ServiceGenericImpl;
+import cn.trymore.core.util.UtilString;
 
 /**
  * The implementation of service of school department position
@@ -58,6 +62,40 @@ extends ServiceGenericImpl<ModelSchoolDepartmentPosition> implements ServiceScho
 			return daoSchoolDepartmentPosition.getPositionByName(positionName);
 		} 
 		catch (Exception e) 
+		{
+			throw new ServiceException(e);
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.shengrui.oa.service.system.ServiceSchoolDepartmentPosition#getPositionsByRole(org.shengrui.oa.model.system.ModelAppRole)
+	 */
+	@Override
+	public List<ModelSchoolDepartmentPosition> getPositionsByRole (ModelAppRole role) 
+			throws ServiceException
+	{
+		try
+		{
+			if (role != null)
+			{
+				DetachedCriteria criteria = DetachedCriteria.forClass(ModelSchoolDepartmentPosition.class);
+				
+				if (UtilString.isNotEmpty(role.getRoleKey()))
+				{
+					criteria.createCriteria("roles").add(Restrictions.eq("roleKey", role.getRoleKey()));
+				}
+				else if (UtilString.isNotEmpty(role.getId()))
+				{
+					criteria.createCriteria("roles").add(Restrictions.eq("id", role.getId()));
+				}
+				
+				return this.daoSchoolDepartmentPosition.getListByCriteria(criteria);
+			}
+			
+			return null;
+		}
+		catch (Exception e)
 		{
 			throw new ServiceException(e);
 		}
